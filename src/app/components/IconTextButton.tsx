e38c9404c363d39a4ca265e5ad521f8efd1f0380
jgdml <jgdml2801@gmail.com>
2024-09-29 14:27:02 -0300

    IconTextButton component

---
+++ src/app/components/IconTextButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import styles from "./IconTextButton.module.css";
+import Link from "next/link";
+
+interface IconTextButtonProps {
+  icon: string;
+  text: string;
+  href?: string;
+  onclick?: () => void;
+  isSubmit?: boolean;
+}
+
+const IconTextButton = (props: IconTextButtonProps) => {
+  if (props.href) {
+    return (
+      <Link href={props.href} className={styles.iconTextButton}>
+        <i className="material-symbols-outlined">{props.icon}</i>
+        {props.text}
+      </Link>
+    );
+  }
+  return (
+    <button
+      onClick={props.onclick}
+      className={styles.iconTextButton}
+      type={props.isSubmit ? "submit" : "button"}
+    >
+      <i className="material-symbols-outlined">{props.icon}</i>
+      {props.text}
+    </button>
+  );
+};
+
+export default IconTextButton;
